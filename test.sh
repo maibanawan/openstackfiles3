4924128584647281d77466f60d4630acac6b786c
mb <37088884+maibanawan@users.noreply.github.com>
2018-03-06 12:24:39 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -5,10 +5,4 @@ echo $OS_TOKEN
 export BLUE_create=$(curl -s http://127.0.0.1:9696/v2.0/networks -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "X-Auth-Token:$OS_TOKEN" -d '{"network": {"name":"BLUE"}}' | python -m json.tool)
 export RED_create=$(curl -s http://127.0.0.1:9696/v2.0/networks -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "X-Auth-Token:$OS_TOKEN" -d '{"network": {"name":"RED"}}' | python -m json.tool > r.json)
 export red=$(cat r.json | jq -r '.network.id')
-if [[ $red =~ ^\{?[A-F0-9a-f]{8}-[A-F0-9a-f]{4}-[A-F0-9a-f]{4}-[A-F0-9a-f]{4}-[A-F0-9a-f]{12}\}?$ ]]; then
-    echo "true"
-else
-    echo "false"
-fi
-export red_sub=$(curl -s http://127.0.0.1:9696/v2.0/subnets -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "X-Auth-Token:$OS_TOKEN" -d '{"subnet":{"name":"Subamp","cidr":"120.1.2.0/24","ip_version":4,"network_id":$red}}' | python -m json.tool)
-curl -s http://127.0.0.1:9696/v2.0/subnets -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "X-Auth-Token:$OS_TOKEN" -d '{"subnet":{"name":"Subamp","cidr":"120.1.2.0/24","ip_version":4,"network_id":"$red"}}' | python -m json.tool
+bash test2.sh $red
