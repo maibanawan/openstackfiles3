88aa102eedd443c2cbcd16de2b30f355335ab9ff
mb <37088884+maibanawan@users.noreply.github.com>
2018-03-06 12:36:14 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,22 @@
-#!/bin/bash
+#!/bin/bash -ex
 import json
 export OS_TOKEN=$(curl -i -H "Content-Type: application/json" -d '{ "auth": {"identity": {"methods": ["password"],"password": {"user": {"name": "admin","domain": { "id": "default" },"password": "secret"}}},"scope": {"project": {"name": "admin","domain": { "id": "default" }}}}}' http://localhost/identity/v3/auth/tokens | awk '/X-Subject-Token/ {print $2}')
 echo $OS_TOKEN
 export BLUE_create=$(curl -s http://127.0.0.1:9696/v2.0/networks -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "X-Auth-Token:$OS_TOKEN" -d '{"network": {"name":"BLUE"}}' | python -m json.tool)
 export RED_create=$(curl -s http://127.0.0.1:9696/v2.0/networks -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "X-Auth-Token:$OS_TOKEN" -d '{"network": {"name":"RED"}}' | python -m json.tool > r.json)
 export red=$(cat r.json | jq -r '.network.id')
-bash test_red.sh $red
+#bash test_red.sh $red
+export RESP_JSON_SUBNETS_CREATE=$(curl -s -X POST http://127.0.0.1:9696/v2.0/subnets \
+            -H "Content-Type: application/json" \
+            -H "X-Auth-Token: $OS_TOKEN" \
+	    -d "{
+			\"subnet\": {
+				\"network_id\": \"$red\",
+				\"ip_version\": 4,
+				\"name\": \"subnet-red\",
+				\"cidr\": \"192.168.1.0/24\",
+				\"enable_dhcp\": true,
+				\"gateway_ip\": \"192.168.100.1\"
+			}
+		}")
+
