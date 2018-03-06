0ae380427a1096a85854b5c694d470c8d6e0bea5
mb <37088884+maibanawan@users.noreply.github.com>
2018-03-06 08:50:27 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,5 +4,5 @@ export OS_TOKEN=$(curl -i -H "Content-Type: application/json" -d '{ "auth": {"id
 echo $OS_TOKEN
 export BLUE_create=$(curl -s http://127.0.0.1:9696/v2.0/networks -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "X-Auth-Token:$OS_TOKEN" -d '{"network": {"name":"BLUE"}}' | python -m json.tool)
 export RED_create=$(curl -s http://127.0.0.1:9696/v2.0/networks -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "X-Auth-Token:$OS_TOKEN" -d '{"network": {"name":"RED"}}' | python -m json.tool)
-echo $BLUE_create['networks'][0][]id]]
+echo $BLUE_create['network'][0][]id]]
 echo $1
