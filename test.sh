d0aa3de5b878213635bf3060b0b116cc3e54c0f5
mb <37088884+maibanawan@users.noreply.github.com>
2018-03-06 12:56:39 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -33,4 +33,7 @@ export RED_SUBNET=$(curl -s -X POST http://127.0.0.1:9696/v2.0/subnets \
 				\"gateway_ip\": \"192.168.100.1\"
 			}
 		}")
+		
+curl -s http://127.0.0.1:9696/v2.0/routers -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "X-Auth-Token:$OS_TOKEN" -d '{"router":{"external_gateway_info":{"network_id":"7697d4c6-5b4c-4ea9-a1d6-af7d7f716f2b"},"name":"router-new"}}' | python -m json.tool		
 
+#curl -s http://127.0.0.1:9696/v2.0/routers/5cd17bbd-5d01-4b71-94e0-67a064250873/add_router_interface -X PUT -H "Content-Type: application/json" -H "Accept: application/json" -H "X-Auth-Token:$OS_TOKEN" -d '{"subnet_id":"06e5b152-37f9-4591-8100-ce2061f081de"}' | python -m json.tool
