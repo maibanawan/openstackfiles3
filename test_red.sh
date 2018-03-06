65fba16b55b16336338a6fe5d24a3176f0a0745e
mb <37088884+maibanawan@users.noreply.github.com>
2018-03-06 12:30:01 -0500

    Update test_red.sh

--- test_red.sh
+++ test_red.sh
@@ -1,10 +1,5 @@
 #!/bin/bash -ex
 
-if [ $# -lt 1 ]; then
-	echo "Usage: $([ -z $BASH_ARGV ] && basename $0 || basename $BASH_ARGV) <network id>"
-	exit 1
-fi
-
 export RESP_JSON_SUBNETS_CREATE=$(curl -s -X POST http://127.0.0.1:9696/v2.0/subnets \
             -H "Content-Type: application/json" \
             -H "X-Auth-Token: $OS_TOKEN" \
