b649c58bdee9dd6e5e51a5689794d5bdffe47df6
mb <37088884+maibanawan@users.noreply.github.com>
2018-03-06 07:30:25 -0500

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export AUTH=$(curl -i -H "Content-Type: application/json" -d '{ "auth": {"identity": {"methods": ["password"],"password": {"user": {"name": "admin","domain": { "id": "default" },"password": "secret"}}},"scope": {"project": {"name": "admin","domain": { "id": "default" }}}}}' http://localhost/identity/v3/auth/tokens)
+export OS_TOKEN=$(echo $AUTH) 
+echo $OS_TOKEN
+export BLUE_create=$(curl -s http://127.0.0.1:9696/v2.0/networks -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "X-Auth-Token:$OS_TOKEN" -d '{"network": {"name":"BLUE"}}' | python -m json.tool)
+export RED_create=$(curl -s http://127.0.0.1:9696/v2.0/networks -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "X-Auth-Token:$OS_TOKEN" -d '{"network": {"name":"RED"}}' | python -m json.tool)
+echo $1
