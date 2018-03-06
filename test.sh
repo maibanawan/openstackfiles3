8df1c7fe8858355515f63260266bcb30430a1d0c
mb <37088884+maibanawan@users.noreply.github.com>
2018-03-06 11:39:36 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -11,4 +11,4 @@ else
     echo "false"
 fi
 export red_sub=$(curl -s http://127.0.0.1:9696/v2.0/subnets -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "X-Auth-Token:$OS_TOKEN" -d '{"subnet":{"name":"Subamp","cidr":"120.1.2.0/24","ip_version":4,"network_id":$red}}' | python -m json.tool)
-curl -s http://127.0.0.1:9696/v2.0/subnets -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "X-Auth-Token:$OS_TOKEN" -d '{"subnet":{"name":"Subamp","cidr":"120.1.2.0/24","ip_version":4,"network_id":$red}}' | python -m json.tool
+curl -s http://127.0.0.1:9696/v2.0/subnets -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "X-Auth-Token:$OS_TOKEN" -d '{"subnet":{"name":"Subamp","cidr":"120.1.2.0/24","ip_version":4,"network_id": $red}}' | python -m json.tool
