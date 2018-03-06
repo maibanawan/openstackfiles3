1dbeaf78d792bc06e1c0ade264141b391f79ef2e
mb <37088884+maibanawan@users.noreply.github.com>
2018-03-06 11:33:00 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,7 +4,12 @@ export OS_TOKEN=$(curl -i -H "Content-Type: application/json" -d '{ "auth": {"id
 echo $OS_TOKEN
 export BLUE_create=$(curl -s http://127.0.0.1:9696/v2.0/networks -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "X-Auth-Token:$OS_TOKEN" -d '{"network": {"name":"BLUE"}}' | python -m json.tool)
 export RED_create=$(curl -s http://127.0.0.1:9696/v2.0/networks -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "X-Auth-Token:$OS_TOKEN" -d '{"network": {"name":"RED"}}' | python -m json.tool > r.json)
-#export red=$(cat r.json | jq -r '.network.id')
-export red=$(curl -s http://127.0.0.1:9696/v2.0/networks/{network_id} -X GET -H "Content-Type: application/json" -H "Accept: application/json" -H "X-Auth-Token:$OS_TOKEN")
-echo $red
+export red=$(cat r.json | jq -r '.network.id')
+if [[ $red =~ ^\{?[A-Z0-9]{8}-[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{12}\}?$ ]]; then
+    echo "true"
+else
+    echo "false"
+fi
+#export red=$(curl -s http://127.0.0.1:9696/v2.0/networks/{network_id} -X GET -H "Content-Type: application/json" -H "Accept: application/json" -H "X-Auth-Token:$OS_TOKEN")
+#echo $red
 #export red_sub=$(curl -s http://127.0.0.1:9696/v2.0/subnets -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "X-Auth-Token:$OS_TOKEN" -d '{"subnet":{"name":"Subamp","cidr":"120.1.2.0/24","ip_version":4,"network_id":"$red"}}' | python -m json.tool)
