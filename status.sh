bf67967cd310d22951aec125106749d062235bda
mb <37088884+maibanawan@users.noreply.github.com>
2018-03-07 07:46:03 -0500

    Update status.sh

--- status.sh
+++ status.sh
@@ -6,6 +6,6 @@ export RESP_JSON_NETWORKS=$(curl -s -X GET http://127.0.0.1:9696/v2.0/networks/$
             -H "Content-Type: application/json" \
             -H "X-Auth-Token: $OS_TOKEN" | python -m json.tool > public_status.json)
 export PUBLIC_status=$(cat public_status.json | jq -r '.networks.status')	
-echo PUBLIC_status
+echo $PUBLIC_status
 
 	    
