d60f700f8df31c88079a42586ca21779d4810ead
mb <37088884+maibanawan@users.noreply.github.com>
2018-03-07 08:36:36 -0500

    Update status.sh

--- status.sh
+++ status.sh
@@ -16,9 +16,9 @@ export net3_name=$(cat network_status.json | jq -r '.networks[3].name')
 export net3_id=$(cat network_status.json | jq -r '.networks[3].id')
 export net3_status=$(cat network_status.json | jq -r '.networks[3].status')
 export OS_TOKEN=${OS_TOKEN//$'\015'}
-export RESP_JSON_VM=$(curl -s -X GET http://127.0.0.1/compute/v2.1/servers/ \
+export RESP_JSON_SERVERS=$(curl -s -X GET http://127.0.0.1/compute/v2.1/servers \
             -H "Content-Type: application/json" \
-            -H "X-Auth-Token: $OS_TOKEN" | python -mjson.tool > vm_status.json)
+            -H "X-Auth-Token: $OS_TOKEN")
 
 
 
