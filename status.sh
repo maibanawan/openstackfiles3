8edd60d6aa6078603fbbc0883b93a25f3173db36
mb <37088884+maibanawan@users.noreply.github.com>
2018-03-07 07:14:45 -0500

    Update status.sh

--- status.sh
+++ status.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -ex
+export OS_TOKEN=$(curl -i -H "Content-Type: application/json" -d '{ "auth": {"identity": {"methods": ["password"],"password": {"user": {"name": "admin","domain": { "id": "default" },"password": "secret"}}},"scope": {"project": {"name": "admin","domain": { "id": "default" }}}}}' http://localhost/identity/v3/auth/tokens | awk '/X-Subject-Token/ {print $2}')
+echo $OS_TOKEN
 export RESP_JSON_NETWORKS=$(curl -s -X GET http://127.0.0.1:9696/v2.0/networks/$public \
             -H "Content-Type: application/json" \
             -H "X-Auth-Token: $OS_TOKEN" | python -m json.tool > public_status.json)
