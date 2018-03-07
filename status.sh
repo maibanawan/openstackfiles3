b5020aa243dfd6e963b468e70c87b1205fae2895
mb <37088884+maibanawan@users.noreply.github.com>
2018-03-07 07:10:38 -0500

    Create status.sh

---
+++ status.sh
@@ -0,0 +1,5 @@
+export RESP_JSON_NETWORKS=$(curl -s -X GET http://127.0.0.1:9696/v2.0/networks/$public \
+            -H "Content-Type: application/json" \
+            -H "X-Auth-Token: $OS_TOKEN" | python -m json.tool > public_status.json)
+echo $RESP_JSON_NETWORKS
+	    
