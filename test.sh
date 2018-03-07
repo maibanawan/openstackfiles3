d87190a01d4a194c0ce95a2e1d0c82fa2e5fc0e0
mb <37088884+maibanawan@users.noreply.github.com>
2018-03-07 18:06:31 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 #import json
 export OS_TOKEN=$(curl -i -H "Content-Type: application/json" -d '{ "auth": {"identity": {"methods": ["password"],"password": {"user": {"name": "admin","domain": { "id": "default" },"password": "secret"}}},"scope": {"project": {"name": "admin","domain": { "id": "default" }}}}}' http://localhost/identity/v3/auth/tokens | awk '/X-Subject-Token/ {print $2}')
 echo $OS_TOKEN
-export PUBLIC_create=$(curl -s http://127.0.0.1:9696/v2.0/networks -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "X-Auth-Token:$OS_TOKEN" -d '{"network": {"name":"PUBLIC","provider:network_type":"GRE","provider:physical_network":"public","router:external":"true"}}' | python -m json.tool > p.json)
+export PUBLIC_create=$(curl -s http://127.0.0.1:9696/v2.0/networks -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "X-Auth-Token:$OS_TOKEN" -d '{"network": {"name":"PUBLIC","provider:network_type":"flat","provider:physical_network":"public","router:external":"true"}}' | python -m json.tool > p.json)
 export BLUE_create=$(curl -s http://127.0.0.1:9696/v2.0/networks -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "X-Auth-Token:$OS_TOKEN" -d '{"network": {"name":"BLUE"}}' | python -m json.tool > b.json)
 export RED_create=$(curl -s http://127.0.0.1:9696/v2.0/networks -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "X-Auth-Token:$OS_TOKEN" -d '{"network": {"name":"RED"}}' | python -m json.tool > r.json)
 export public=$(cat p.json | jq -r '.network.id')
@@ -19,7 +19,7 @@ export PUBLIC_SUBNET=$(curl -s -X POST http://127.0.0.1:9696/v2.0/subnets \
 				\"name\": \"subnet-public\",
 				\"cidr\": \"172.24.4.0/24\",
 				\"enable_dhcp\": true,
-				\"gateway_ip\": \"172.24.4.1\"
+				\"gateway_ip\": \"none\"
 			}
 		}" | python -m json.tool > psub.json)
 export psubid=$(cat psub.json | jq -r '.subnet.id')	
