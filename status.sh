b31da20bdc6e37e3f9d665bbd98318b396a25f96
mb <37088884+maibanawan@users.noreply.github.com>
2018-03-07 08:17:01 -0500

    Update status.sh

--- status.sh
+++ status.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -ex
-echo $public
+
 export OS_TOKEN=$(curl -i -H "Content-Type: application/json" -d '{ "auth": {"identity": {"methods": ["password"],"password": {"user": {"name": "admin","domain": { "id": "default" },"password": "secret"}}},"scope": {"project": {"name": "admin","domain": { "id": "default" }}}}}' http://localhost/identity/v3/auth/tokens | awk '/X-Subject-Token/ {print $2}')
 echo $OS_TOKEN
 #jq 'del(.networks)' public_status.json
-export RESP_JSON_NETWORKS=$(curl -s -X GET http://127.0.0.1:9696/v2.0/networks/$public \
+export RESP_JSON_NETWORKS=$(curl -s -X GET http://127.0.0.1:9696/v2.0/networks/ \
             -H "Content-Type: application/json" \
-            -H "X-Auth-Token: $OS_TOKEN" | python -mjson.tool > public_status.json)
-export PUBLIC_status=$(cat public_status.json | jq -r '.network.status')	
-echo $PUBLIC_status
+            -H "X-Auth-Token: $OS_TOKEN" | python -mjson.tool > network_status.json)
+#export PUBLIC_status=$(cat public_status.json | jq -r '.network.status')	
+#echo $PUBLIC_status
 
 	    
