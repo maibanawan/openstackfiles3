c87f065406fa86c172a6d415f84f29d70e6b9010
mb <37088884+maibanawan@users.noreply.github.com>
2018-03-07 16:14:34 -0800

    Create testcopy.sh

---
+++ testcopy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -ex
+#import json
+export OS_TOKEN=$(curl -i -H "Content-Type: application/json" -d '{ "auth": {"identity": {"methods": ["password"],"password": {"user": {"name": "admin","domain": { "id": "default" },"password": "secret"}}},"scope": {"project": {"name": "admin","domain": { "id": "default" }}}}}' http://localhost/identity/v3/auth/tokens | awk '/X-Subject-Token/ {print $2}')
+echo $OS_TOKEN
+export PUBLIC_create=$(curl -s http://127.0.0.1:9696/v2.0/networks -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "X-Auth-Token:$OS_TOKEN" -d '{"network": {"name":"PUBLIC","provider:network_type":"flat","provider:physical_network":"public","router:external":"true"}}' | python -m json.tool > p.json)
+export BLUE_create=$(curl -s http://127.0.0.1:9696/v2.0/networks -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "X-Auth-Token:$OS_TOKEN" -d '{"network": {"name":"BLUE"}}' | python -m json.tool > b.json)
+export RED_create=$(curl -s http://127.0.0.1:9696/v2.0/networks -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "X-Auth-Token:$OS_TOKEN" -d '{"network": {"name":"RED"}}' | python -m json.tool > r.json)
+export public=$(cat p.json | jq -r '.network.id')
+export blue=$(cat b.json | jq -r '.network.id')
+export red=$(cat r.json | jq -r '.network.id')
+#bash test_red.sh $red
+
+export RESP_JSON_ROUTERS_CREATE=$(curl -s -X POST http://127.0.0.1:9696/v2.0/routers \
+            -H "Content-Type: application/json" \
+            -H "X-Auth-Token: $OS_TOKEN" \
+	    -d "{
+			\"router\": {
+				\"external_gateway_info\": {
+					\"network_id\": \"$public\"
+				},
+				\"name\": \"router-new\"
+			}
+		}" | python -m json.tool > rout.json)
