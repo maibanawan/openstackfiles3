e70f9d3ab81b8063885e83589d77bae80547bc09
mb <37088884+maibanawan@users.noreply.github.com>
2018-03-07 21:10:49 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -4,16 +4,13 @@ export OS_TOKEN=$(curl -i -H "Content-Type: application/json" -d '{ "auth": {"id
 echo $OS_TOKEN
 HOST_ROUTES_AZ1={\”nexthop\”:\”$172.24.4.8\”,\”destination\”:\”10.0.0.0/24\”}
 export x=$(curl -k -X PUT http://127.0.0.1:9696/v2.0/subnets/b5f13dd8-b273-46c0-9472-e31feb444b22\
-         -H "X-Auth-Token: $OS_TOKEN"\
-         -H "Content-Type: application/json"\ 
-	 -d "{
-			  \"subnet\": {
-				     \"host_routes\": [
-                                                  \"nexthop\":\"172.24.4.8\",
-						  \"destination\":\"10.0.0.0/24\"
-                                     ] 
-			  }
-	      }" | python -m json.tool)
+           -H "X-Auth-Token: $OS_TOKEN"\
+           -H "Content-Type: application/json"\ 
+	   -d "{
+		    "subnet": {
+			"host_routes": [{"nexthop":"172.24.4.8","destination":"10.0.0.0/24"}],
+		    }
+		}" | python -m json.tool)
 
 
 
