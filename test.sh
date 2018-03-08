87b517bb46e25d99f9e48c3e53ad64c3fc24b1e7
mb <37088884+maibanawan@users.noreply.github.com>
2018-03-08 03:45:16 -0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,16 +1,17 @@
 #!/bin/bash -ex
 #import json
+export loacalhost=$1
 export OS_TOKEN=$(curl -i -H "Content-Type: application/json" -d '{ "auth": {"identity": {"methods": ["password"],"password": {"user": {"name": "admin","domain": { "id": "default" },"password": "secret"}}},"scope": {"project": {"name": "admin","domain": { "id": "default" }}}}}' http://localhost/identity/v3/auth/tokens | awk '/X-Subject-Token/ {print $2}')
 echo $OS_TOKEN
-export PUBLIC_create=$(curl -s http://127.0.0.1:9696/v2.0/networks -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "X-Auth-Token:$OS_TOKEN" -d '{"network": {"name":"PUBLIC","provider:network_type":"flat","provider:physical_network":"public","router:external":"true"}}' | python -m json.tool > p.json)
-export BLUE_create=$(curl -s http://127.0.0.1:9696/v2.0/networks -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "X-Auth-Token:$OS_TOKEN" -d '{"network": {"name":"BLUE"}}' | python -m json.tool > b.json)
-export RED_create=$(curl -s http://127.0.0.1:9696/v2.0/networks -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "X-Auth-Token:$OS_TOKEN" -d '{"network": {"name":"RED"}}' | python -m json.tool > r.json)
+export PUBLIC_create=$(curl -s http://localhost:9696/v2.0/networks -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "X-Auth-Token:$OS_TOKEN" -d '{"network": {"name":"PUBLIC","provider:network_type":"flat","provider:physical_network":"public","router:external":"true"}}' | python -m json.tool > p.json)
+export BLUE_create=$(curl -s http://localhost:9696/v2.0/networks -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "X-Auth-Token:$OS_TOKEN" -d '{"network": {"name":"BLUE"}}' | python -m json.tool > b.json)
+export RED_create=$(curl -s http://localhost:9696/v2.0/networks -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "X-Auth-Token:$OS_TOKEN" -d '{"network": {"name":"RED"}}' | python -m json.tool > r.json)
 export public=$(cat p.json | jq -r '.network.id')
 export blue=$(cat b.json | jq -r '.network.id')
 export red=$(cat r.json | jq -r '.network.id')
 #bash test_red.sh $red
 
-export PUBLIC_SUBNET=$(curl -s -X POST http://127.0.0.1:9696/v2.0/subnets \
+export PUBLIC_SUBNET=$(curl -s -X POST http://localhost:9696/v2.0/subnets \
             -H "Content-Type: application/json" \
             -H "X-Auth-Token: $OS_TOKEN" \
             -d "{
@@ -25,7 +26,7 @@ export PUBLIC_SUBNET=$(curl -s -X POST http://127.0.0.1:9696/v2.0/subnets \
 		}" | python -m json.tool > psub.json)
 export psubid=$(cat psub.json | jq -r '.subnet.id')	
 echo $psubid
-export BLUE_SUBNET=$(curl -s -X POST http://127.0.0.1:9696/v2.0/subnets \
+export BLUE_SUBNET=$(curl -s -X POST http://localhost:9696/v2.0/subnets \
             -H "Content-Type: application/json" \
             -H "X-Auth-Token: $OS_TOKEN" \
 	    -d "{
@@ -40,7 +41,7 @@ export BLUE_SUBNET=$(curl -s -X POST http://127.0.0.1:9696/v2.0/subnets \
 		}" | python -m json.tool > bsub.json)
 export bsubid=$(cat bsub.json | jq -r '.subnet.id')	
 echo $bsubid
-export RED_SUBNET=$(curl -s -X POST http://127.0.0.1:9696/v2.0/subnets \
+export RED_SUBNET=$(curl -s -X POST http://localhost:9696/v2.0/subnets \
             -H "Content-Type: application/json" \
             -H "X-Auth-Token: $OS_TOKEN" \
 	    -d "{
@@ -56,7 +57,7 @@ export RED_SUBNET=$(curl -s -X POST http://127.0.0.1:9696/v2.0/subnets \
 export rsubid=$(cat rsub.json | jq -r '.subnet.id')
 echo $rsubid
 #curl -s http://127.0.0.1:9696/v2.0/routers -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "X-Auth-Token:$OS_TOKEN" -d '{"router":{"external_gateway_info":{"network_id":"11cfdf0d-2b10-41d8-a919-efdaabddb08e"},"name":"router-new"}}' | python -m json.tool > rout.json		
-export RESP_JSON_ROUTERS_CREATE=$(curl -s -X POST http://127.0.0.1:9696/v2.0/routers \
+export RESP_JSON_ROUTERS_CREATE=$(curl -s -X POST http://localhost:9696/v2.0/routers \
             -H "Content-Type: application/json" \
             -H "X-Auth-Token: $OS_TOKEN" \
 	    -d "{
@@ -76,15 +77,15 @@ export RESP_JSON_ROUTERS_CREATE=$(curl -s -X POST http://127.0.0.1:9696/v2.0/rou
 export router=$(cat rout.json | jq -r '.router.id')
 export ext_gate=$(cat rout.json | jq -r '.router.external_gateway_info.external_fixed_ips[0].ip_address')
 echo $router
-export xx=$(curl -k -X PUT http://127.0.0.1:9696/v2.0/subnets/$psubid -H "Content-Type: application/json" -H "X-Auth-Token: $OS_TOKEN" -d '{"subnet":{"host_routes":[{"destination":"10.0.0.0/24","nexthop":"172.24.4.9"},{"destination":"192.168.1.0/24","nexthop":"172.24.4.9"}]}}')
+export xx=$(curl -k -X PUT http://localhost:9696/v2.0/subnets/$psubid -H "Content-Type: application/json" -H "X-Auth-Token: $OS_TOKEN" -d '{"subnet":{"host_routes":[{"destination":"10.0.0.0/24","nexthop":"172.24.4.9"},{"destination":"192.168.1.0/24","nexthop":"172.24.4.9"}]}}')
 
-export ADD_ROUTER_IF1=$(curl -s -X PUT http://127.0.0.1:9696/v2.0/routers/$router/add_router_interface \
+export ADD_ROUTER_IF1=$(curl -s -X PUT http://localhost:9696/v2.0/routers/$router/add_router_interface \
             -H "Content-Type: application/json" \
             -H "X-Auth-Token: $OS_TOKEN" \
 	    -d "{
 			\"subnet_id\": \"$bsubid\"
 		}")
-export ADD_ROUTER_IF2=$(curl -s -X PUT http://127.0.0.1:9696/v2.0/routers/$router/add_router_interface \
+export ADD_ROUTER_IF2=$(curl -s -X PUT http://localhost:9696/v2.0/routers/$router/add_router_interface \
             -H "Content-Type: application/json" \
             -H "X-Auth-Token: $OS_TOKEN" \
        	    -d "{
@@ -114,12 +115,12 @@ export OS_TOKEN=${OS_TOKEN//$'\015'}
 #	            \"networks\": \"$blue\",
 #	           }
 #   }"
-curl -X POST "http://127.0.0.1/compute/v2.1/servers" -H "Content-Type: application/json" -H "X-Auth-Token: $OS_TOKEN" -d "{\"server\":{\"name\":\"vm1\",\"imageRef\":\"aaab4dfd-8d9c-409e-a821-a0137e49e869\", \"flavorRef\":42, \"networks\": [{\"uuid\": \"$blue\"}]}}"
-curl -X POST "http://127.0.0.1/compute/v2.1/servers" -H "Content-Type: application/json" -H "X-Auth-Token: $OS_TOKEN" -d "{\"server\":{\"name\":\"vm2\",\"imageRef\":\"aaab4dfd-8d9c-409e-a821-a0137e49e869\", \"flavorRef\":42, \"networks\": [{\"uuid\": \"$red\"}]}}"
-curl -X POST "http://127.0.0.1/compute/v2.1/servers" -H "Content-Type: application/json" -H "X-Auth-Token: $OS_TOKEN" -d "{\"server\":{\"name\":\"vm3\",\"imageRef\":\"aaab4dfd-8d9c-409e-a821-a0137e49e869\", \"flavorRef\":42, \"networks\": [{\"uuid\": \"$public\"}]}}"
+curl -X POST "http://localhost/compute/v2.1/servers" -H "Content-Type: application/json" -H "X-Auth-Token: $OS_TOKEN" -d "{\"server\":{\"name\":\"vm1\",\"imageRef\":\"aaab4dfd-8d9c-409e-a821-a0137e49e869\", \"flavorRef\":42, \"networks\": [{\"uuid\": \"$blue\"}]}}"
+curl -X POST "http://localhost/compute/v2.1/servers" -H "Content-Type: application/json" -H "X-Auth-Token: $OS_TOKEN" -d "{\"server\":{\"name\":\"vm2\",\"imageRef\":\"aaab4dfd-8d9c-409e-a821-a0137e49e869\", \"flavorRef\":42, \"networks\": [{\"uuid\": \"$red\"}]}}"
+curl -X POST "http://localhost/compute/v2.1/servers" -H "Content-Type: application/json" -H "X-Auth-Token: $OS_TOKEN" -d "{\"server\":{\"name\":\"vm3\",\"imageRef\":\"aaab4dfd-8d9c-409e-a821-a0137e49e869\", \"flavorRef\":42, \"networks\": [{\"uuid\": \"$public\"}]}}"
 
 #curl -X POST "http://172.0.0.1:8774/v1.1/5/os-security-group-rules" -H "X-Auth-Token: $OS_TOKEN" -H "Content-Type: application/json" -d "{\"security_group_rule\":{\"ip_protocol\":\"tcp\",\"from_port\":\"22\",\"to_port\":\"22\",\"cidr\":\"0.0.0.0/0\"}}" 
-export RESP_JSON_SECURITY_GROUP_RULES_CREATE=$(curl -s -X POST http://127.0.0.1:9696/v2.0/security-group-rules \
+export RESP_JSON_SECURITY_GROUP_RULES_CREATE=$(curl -s -X POST http://localhost:9696/v2.0/security-group-rules \
             -H "Content-Type: application/json" \
             -H "X-Auth-Token: $OS_TOKEN" \
 	    -d "{
@@ -132,7 +133,7 @@ export RESP_JSON_SECURITY_GROUP_RULES_CREATE=$(curl -s -X POST http://127.0.0.1:
 			}
 		}")
 
-export RESP_JSON_SECURITY_GROUP_RULES_CREATE=$(curl -s -X POST http://127.0.0.1:9696/v2.0/security-group-rules \
+export RESP_JSON_SECURITY_GROUP_RULES_CREATE=$(curl -s -X POST http://localhost:9696/v2.0/security-group-rules \
             -H "Content-Type: application/json" \
             -H "X-Auth-Token: $OS_TOKEN" \
 	    -d "{
@@ -144,7 +145,7 @@ export RESP_JSON_SECURITY_GROUP_RULES_CREATE=$(curl -s -X POST http://127.0.0.1:
 				\"ethertype\": \"IPv4\"
 			}
 		}")
-export RESP_JSON_SECURITY_GROUP_RULES_CREATE=$(curl -s -X POST http://127.0.0.1:9696/v2.0/security-group-rules \
+export RESP_JSON_SECURITY_GROUP_RULES_CREATE=$(curl -s -X POST http://localhost:9696/v2.0/security-group-rules \
             -H "Content-Type: application/json" \
             -H "X-Auth-Token: $OS_TOKEN" \
 	    -d "{
@@ -156,7 +157,7 @@ export RESP_JSON_SECURITY_GROUP_RULES_CREATE=$(curl -s -X POST http://127.0.0.1:
 				\"ethertype\": \"IPv4\"
 			}
 		}")
-export RESP_JSON_SECURITY_GROUP_RULES_CREATE=$(curl -s -X POST http://127.0.0.1:9696/v2.0/security-group-rules \
+export RESP_JSON_SECURITY_GROUP_RULES_CREATE=$(curl -s -X POST http://localhost:9696/v2.0/security-group-rules \
             -H "Content-Type: application/json" \
             -H "X-Auth-Token: $OS_TOKEN" \
 	    -d "{
