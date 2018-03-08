cb89898524a52d499b8f025c3206d467cc5b4969
mb <37088884+maibanawan@users.noreply.github.com>
2018-03-08 04:47:41 -0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -115,6 +115,16 @@ export OS_TOKEN=${OS_TOKEN//$'\015'}
 #	            \"networks\": \"$blue\",
 #	           }
 #   }"
+
+export SECURITY_GROUPS_CREATE=$(curl -s -X POST http://localhost:9696/v2.0/security-groups \
+            -H "Content-Type: application/json" \
+            -H "X-Auth-Token: $OS_TOKEN" \
+	    -d "{
+			\"security_group\": {
+				\"name\": \"mysg\",
+			}
+		}" | python -mjson.tool > sg.json)
+		
 curl -X POST "http://localhost/compute/v2.1/servers" -H "Content-Type: application/json" -H "X-Auth-Token: $OS_TOKEN" -d "{\"server\":{\"name\":\"vm1\",\"imageRef\":\"aaab4dfd-8d9c-409e-a821-a0137e49e869\", \"flavorRef\":42, \"networks\": [{\"uuid\": \"$blue\"}]}}"
 curl -X POST "http://localhost/compute/v2.1/servers" -H "Content-Type: application/json" -H "X-Auth-Token: $OS_TOKEN" -d "{\"server\":{\"name\":\"vm2\",\"imageRef\":\"aaab4dfd-8d9c-409e-a821-a0137e49e869\", \"flavorRef\":42, \"networks\": [{\"uuid\": \"$red\"}]}}"
 curl -X POST "http://localhost/compute/v2.1/servers" -H "Content-Type: application/json" -H "X-Auth-Token: $OS_TOKEN" -d "{\"server\":{\"name\":\"vm3\",\"imageRef\":\"aaab4dfd-8d9c-409e-a821-a0137e49e869\", \"flavorRef\":42, \"networks\": [{\"uuid\": \"$public\"}]}}"
